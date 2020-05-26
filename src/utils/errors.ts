f4bca4ddf36772efb0d0da3fec127cdd63fdd295
Sebastien DErrico <sebastien@hollox.net>
2020-05-26 07:52:32 -0400

    Adjust error output

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -10,6 +10,8 @@ export const handleErrors = <T>(
   controller: (req: Request, res: Response) => Promise<T>
 ) => (req: Request, res: Response, next: NextFunction): Promise<T | void> => {
   return controller(req, res).catch(e => {
+    // eslint-disable-next-line no-console
+    console.log(e);
     logger.error(e);
     next(e);
   });
